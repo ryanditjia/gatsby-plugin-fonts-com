d11cab0307fe05e8881ea85385a3bebd47381ae8
Ryandi Tjia <ryandi.tjia@gmail.com>
2018-07-26 19:48:14 +0700

    Fix src so that it’s always relative to root

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -8,7 +8,7 @@ exports.onRenderBody = ({ setHeadComponents }, { projectId }) => {
       var mtiTracking = document.createElement('script');
       mtiTracking.type = 'text/javascript';
       mtiTracking.async = 'true';
-      mtiTracking.src = 'mtiFontTrackingCode.js';
+      mtiTracking.src = '/mtiFontTrackingCode.js';
       (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(mtiTracking);
   })();`
 
